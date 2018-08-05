e1cb6a7d86f51cb318255ebfc8a5ffa225d49d98
Manuk Karapetyan <kmanuk@MacBook-Pro-Manuk.local>
2018-08-05 18:26:03 +0300

    The date added

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,8 @@
-class Project
-  attr_accessor :tasks
+class Project < ApplicationRecord
+  has_many :tasks, dependent: :destroy
 
-  def initialize
-    @tasks = []
+  def self.velocity_length_in_days
+    21
   end
 
   def incomplete_tasks
@@ -19,5 +19,22 @@ class Project
 
   def remaining_size
     incomplete_tasks.sum(&:size)
+  end
+
+  def completed_velocity
+    tasks.sum(&:points_toward_velocity)
+  end
+
+  def current_rate
+    completed_velocity * 1.0 / Project.velocity_length_in_days
+  end
+
+  def projected_days_remaining
+    remaining_size / current_rate
+  end
+
+  def on_schedule?
+    return false if projected_days_remaining.nan?
+    (Time.zone.today + projected_days_remaining) <= due_date
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,16 +1,20 @@
-class Task
-  attr_accessor :size, :completed
+class Task < ApplicationRecord
+  belongs_to :project
 
-  def initialize(options = {})
-    @completed = options[:completed]
-    @size = options[:size]
-  end
-
-  def mark_completed
-    @completed = true
+  def mark_completed(date = Time.current)
+    self.completed_at = date
   end
 
   def complete?
-    @completed
+    completed_at.present?
+  end
+
+  def part_of_velocity?
+    return false unless complete?
+    completed_at > Project.velocity_length_in_days.days.ago
+  end
+
+  def points_toward_velocity
+    part_of_velocity? ? size : 0
   end
 end
